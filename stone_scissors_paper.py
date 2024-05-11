fb412c32c8962de3e03ad80b1264e20f3f111468
Temurlan01 <erkinbaevtemurlan@gmail.com>
2024-05-11 20:04:28 +0600

    Теленрамм бот камень ножницы бумага

--- stone_scissors_paper.py
+++ stone_scissors_paper.py
@@ -1,3 +1,4 @@
+import random
 import telebot
 from local_setings import API_TOKEN1
 
@@ -7,14 +8,28 @@ bot = telebot.TeleBot(API_TOKEN1)
 
 @bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, """\
-Hi there, I am EchoBot.
-I am here to echo your kind words back to you. Just say anything nice and I'll say the exact same thing to you!\
-""")
+    bot.reply_to(message, "Добро пожаловать в 'игру камень ножницы бумага'! " 
+                               "Выберите камень (к), ножницы (н) или бумагу" )
 
 @bot.message_handler(func=lambda message: True)
-def echo_message(message):
-    bot.reply_to(message, message.text)
+def play_game(message):
+    user_move = message.text
+    user_move = user_move.lower()
+    if user_move not in ["к", "н", "б"]:
+        bot.reply_to(message, "Пожалуйста введите либо к, н или б.")
+    #bot.reply_to(message, message.text)
+    else:
+        computer_move = random.choice(["к", "н", "б"])
+        if (user_move == "к" and computer_move == "н") or \
+                (user_move == "н" and computer_move == "б") or \
+                (user_move == "б" and computer_move == "к") :
+            bot.reply_to(message, "Вы выиграли!")
+        elif user_move == computer_move:
+            bot.reply_to(message, "Ничья!")
+        else:
+            bot.reply_to(message, "Бот выиграл!")
+
+
 
 
 bot.infinity_polling()
